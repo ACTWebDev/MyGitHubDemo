de56b731cf6107451efb5385d27e118a77d5627a
Steve Rossiter <Steve.Rossiter@ancoraeducation.com>
2020-01-15 15:58:03 -0700

    Updated.

--- GitHubDemo/Program.cs
+++ GitHubDemo/Program.cs
@@ -7,6 +7,7 @@ namespace GitHubDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Updated and pushed to GitHub");
         }
     }
 }
